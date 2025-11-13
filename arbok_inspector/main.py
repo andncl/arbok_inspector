8db5af5ff58d2664c409a6397fadfb44574d3e7a
andncl <andreaskochnickl@web.de>
2025-11-13 01:12:38 +0000

    Simplified main and made windows compatible

--- arbok_inspector/main.py
+++ arbok_inspector/main.py
@@ -1,15 +1,13 @@
 from nicegui import ui
-from pathlib import Path
-import asyncio
-from typing import Optional
 
-from arbok_inspector.state import inspector
-from arbok_inspector.pages import greeter, database_browser
+def run():
+    ui.run(
+        title='Arbok Inspector',
+        favicon='🐍',
+        dark=True,
+        show=True,
+        port=8090
+    )
 
-ui.run(
-    title='Arbok Inspector',
-    favicon='🐍',
-    dark=True,
-    show=True,
-    port=8090
-)
+if __name__ in {"__main__", "__mp_main__", "arbok_inspector.main"}:
+    run()
