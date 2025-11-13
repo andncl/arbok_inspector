9b4a791747ce3eb7ccdbf552bb496a1cff6d9ff0
andncl <andreaskochnickl@web.de>
2025-11-13 05:05:31 +0000

    Reworked CLI launch options

--- arbok_inspector/dev.py
+++ arbok_inspector/dev.py
@@ -1,19 +1,17 @@
 from nicegui import ui
-from pathlib import Path
-import asyncio
-from typing import Optional
 
 from arbok_inspector.state import inspector
 from arbok_inspector.pages import greeter, database_browser
 
-def main():
+def run():
     ui.run(
         title='Arbok Inspector',
         favicon='🐍',
         dark=True,
         show=True,
-        port=8090
+        port=8090,
+        reload = True
     )
 
 if __name__ in {"__main__", "__mp_main__"}:
-    main()
+    run()
--- arbok_inspector/main.py
+++ arbok_inspector/main.py
@@ -1,8 +1,4 @@
 from nicegui import ui
-from nicegui import ui
-from pathlib import Path
-import asyncio
-from typing import Optional
 
 from arbok_inspector.state import inspector
 from arbok_inspector.pages import greeter, database_browser
@@ -13,8 +9,9 @@ def run():
         favicon='🐍',
         dark=True,
         show=True,
-        port=8090
+        port=8090,
+        reload = False
     )
 
-if __name__ in {"__main__", "__mp_main__", "arbok_inspector.main"}:
+if __name__ in {"__main__", "__mp_main__"}:
     run()
