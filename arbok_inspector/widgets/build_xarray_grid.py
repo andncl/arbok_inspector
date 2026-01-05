e7b77a8c7d02e0f78e67b1194d9f838acf0b8992
andncl <andreaskochnickl@web.de>
2026-01-05 05:47:44 +0000

    Overhauled ployly figure building for arrays with different dims

--- arbok_inspector/widgets/build_xarray_grid.py
+++ arbok_inspector/widgets/build_xarray_grid.py
@@ -13,9 +13,9 @@ from arbok_inspector.helpers.string_formaters import (
 )
 
 if TYPE_CHECKING:
-    from arbok_inspector.classes.dim import Dim
     from arbok_inspector.classes.base_run import BaseRun
     from plotly.graph_objs import Figure
+    from xarray import DataArray
 
 def build_xarray_grid(has_new_data: bool = False) -> None:
     """
@@ -34,96 +34,120 @@ def build_xarray_grid(has_new_data: bool = False) -> None:
             color = 'red')
         return
     ds = run.generate_subset(has_new_data=has_new_data)
-    if len(ds.dims) == 1:
-        create_1d_plot(run, ds, container)
-    elif len(ds.dims) == 2:
-        create_2d_grid(run, ds, container)
-    else:
-        ui.notify(
-            'The selected dimensions result in more than 2D data.<br>'
-            'Please select only 1 or 2 dimensions to plot)',
-            color = 'red')
+    results_1d = {}
+    results_2d = {}
+    results_unshowable = {}
+    for result_name in run.plot_selection:
+        result = ds[result_name]
+        if len(result.dims) == 1:
+            results_1d[result_name] = result
+        elif len(result.dims) == 2:
+            results_2d[result_name] = result
+        else:
+            results_unshowable[result_name] = result
 
-def create_1d_plot(run: BaseRun, ds: xr.Dataset, container: ui.Row) -> None:
+    figures: list = create_1d_plot(run, results_1d)
+    figures += create_2d_plots(run, results_2d)
+    create_figures_ui_grid(figures, container, run)
+
+def create_1d_plot(run: BaseRun, results_dict: dict[str, DataArray]) -> Figure:
     """
-    Create a 1D plot for the given run and dataset.
-
+    Creates plotly figure with all 1D traces in it.
+    
     Args:
-        run: The Run object containing the data to plot.
-        ds: The xarray Dataset containing the data.
-        container: The NiceGUI container to hold the plot.
+        run (RunBase): Run that data is taken from
+        results_dict (dict): Dict with result names as keys and xarray DataArrays
+            as keys
+
+    Returns:
+        plotly figure
     """
     print("Creating 1D plot")
     x_dim = run.dim_axis_option['x-axis'].name
     traces = []
     plot_dict = copy.deepcopy(app.storage.tab["plot_dict_1D"])
-    for key in run.plot_selection:
-        da = ds[key]
-        traces.append({
-            "type": "scatter",
-            "mode": "lines+markers",
-            "name": key.replace("__", "."),
-            "x": da.coords[x_dim].values.tolist(),
-            "y": da.values.tolist(),
-        })
-
+    for result_name, result in results_dict.items():
+        if x_dim in result.coords:
+            traces.append({
+                "type": "scatter",
+                "mode": "lines+markers",
+                "name": result_name.replace("__", "."),
+                "x": result.coords[x_dim].values.tolist(),
+                "y": result.values.tolist(),
+            })
+            plot_dict["layout"]["xaxis"]["title"]["text"] = axis_label_formater(
+                result, x_dim)
+ 
+        else:
+            ui.notify(
+                f"Result {result_name} does not have coordinates for {x_dim}",
+                type = "negative"
+            )
     plot_dict["data"] = traces
-    plot_dict["layout"]["xaxis"]["title"]["text"] = axis_label_formater(ds, x_dim)
     plot_dict = add_title_to_plot_dict(run, plot_dict, None)
+    if traces:
+        return [go.Figure(plot_dict)]
+    else:
+        return []
 
-    with container:
-        fig = go.Figure(plot_dict)
-        run.figures = [fig]
-        plot = ui.plotly(fig).classes('flex-1 h-full w-full').style('height:100%; min-height:700px;')
-        run.plots = [plot]
-        app.storage.tab["plot_dict_1D"] = plot_dict
-
-def create_2d_grid(run: BaseRun, ds, container) -> dict:
+def create_2d_plots(
+        run: BaseRun, results_dict: dict[str, DataArray]) -> list[Figure]:
     """
-    Create a grid of 2D plots for the given run and dataset.
+    Creates a list with all plotly 2D plots from the given data dict
+    
+    Args:
+        run (BaseRun): Run object describing measurement
+        results_dict (dict): Dict with result names as keys and xarray
+            DataArrays as values
+    """
+    run = app.storage.tab["run"]
+    figures_2d = []
+    for result_name, result in results_dict.items():
+        figure = create_2d_figure(result_name, result, run)
+        figures_2d.append(figure)
+    return figures_2d
+
+def create_2d_figure(
+        result_name: str, result: DataArray, run: BaseRun) -> Figure:
+    """
+    Creates single 2D plotly figure for the given result
 
     Args:
-        run: The Run object containing the data to plot.
-        ds: The xarray Dataset containing the data.
-        container: The NiceGUI container to hold the plots.
+        result_name (str): Name of result
+        result (DataArray): xarray DataArray to display
+        run (BaseRun): Run object of measurement
     """
-    print("Creating 2D grid of plots")
-    if not all([run.dim_axis_option[axis]is not None for axis in ['x-axis', 'y-axis']]):
-        ui.notify(
-            'Please select both x-axis and y-axis dimensions to display 2D plots.<br>'
-            f'x: {run.dim_axis_option["x-axis"]}<br>'
-            f'y: {run.dim_axis_option["y-axis"]}',
-            color = 'red')
-        return
-    keys = run.plot_selection
-    num_plots = len(keys)
-    num_columns = int(min([run.plots_per_column, len(keys)]))
-    num_rows = math.ceil(num_plots / num_columns)
-    pretty_keys = [key.replace("__", ".") for key in keys]
-
     x_dim = run.dim_axis_option['x-axis'].name
     y_dim = run.dim_axis_option['y-axis'].name
     plot_dict = copy.deepcopy(app.storage.tab["plot_dict_2D"])
-    plot_dict["layout"]["xaxis"]["title"]["text"] = axis_label_formater(ds, x_dim)
-    plot_dict["layout"]["yaxis"]["title"]["text"] = axis_label_formater(ds, y_dim)
+    plot_dict["layout"]["xaxis"]["title"]["text"] = axis_label_formater(
+        result, x_dim)
+    plot_dict["layout"]["yaxis"]["title"]["text"] = axis_label_formater(
+        result, y_dim)
     plot_dict["layout"]["yaxis"]["automargin"] = True
     plot_dict["layout"]["xaxis"]["automargin"] = True
+    if result[x_dim].dims[0] != result.dims[1]:
+        result = result.transpose()
+    plot_dict["data"][0]["z"] = result.values.tolist()
+    plot_dict["data"][0]["x"] = result.coords[x_dim].values.tolist()
+    plot_dict["data"][0]["y"] = result.coords[y_dim].values.tolist()
+    title = result_name.replace("__", ".")
+    plot_dict = add_title_to_plot_dict(run, plot_dict, title)
+    return go.Figure(plot_dict)
+
+def create_figures_ui_grid(figures: list[Figure], container, run: BaseRun) -> None:
+    """
+    Generates a grid of plotly figures in the given ui container
+
+    Args:
+        figures (list): List of plotly figures to display
+        container: UI container to display figures in
+        run (BaseRun): Run object for measurement
+    """
+    num_plots = len(figures)
+    num_columns = int(min([run.plots_per_column, len(figures)]))
+    num_rows = math.ceil(num_plots / num_columns)
     plot_idx = 0
-    def create_2d_plot(plot_dict, plot_idx):
-        key = keys[plot_idx]
-        da = ds[key]
-        if da[x_dim].dims[0] != da.dims[1]:
-            # TODO: CHECK THIS!
-            da = da.transpose() #y_dim, x_dim)
-        plot_dict["data"][0]["z"] = da.values.tolist()
-        plot_dict["data"][0]["x"] = da.coords[x_dim].values.tolist()
-        plot_dict["data"][0]["y"] = da.coords[y_dim].values.tolist()
-        plot_dict = add_title_to_plot_dict(run, plot_dict, pretty_keys[plot_idx])
-
-        return go.Figure(plot_dict)
-
-    run.plots = []
-    run.figures = []
     with container:
         with ui.column().classes('w-full h-full'):
             for row in range(num_rows):
@@ -131,19 +155,16 @@ def create_2d_grid(run: BaseRun, ds, container) -> dict:
                     for col in range(num_columns):
                         if plot_idx >= num_plots:
                             break
-                        fig = create_2d_plot(plot_dict,plot_idx)
-                        run.figures.append(fig)
                         width_percent = 100 / num_columns - 2
                         height_percent = 100 / num_rows - 2
                         with ui.column().style(
                             f"width: {width_percent}%; box-sizing: border-box;"
                             f"height: {height_percent}%; box-sizing: border-box;"
                             ):
-                            plot = ui.plotly(fig).classes('w-full h-full')\
+                            ui.plotly(figures[plot_idx])\
+                                .classes('w-full h-full')\
                                 .style(f'min-height: {int(800/num_rows)}px;')
-                            run.plots.append(plot)
                         plot_idx += 1
-    app.storage.tab["plot_dict_2D"] = plot_dict
 
 def add_title_to_plot_dict(run: BaseRun, plot_dict: dict, result_name: str) -> dict:
     """
