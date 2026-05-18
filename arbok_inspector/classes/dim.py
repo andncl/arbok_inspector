5953331f9cf726b58b56dd019f752621073b48e9
andncl <andreaskochnickl@web.de>
2026-05-18 12:18:31 +0000

    Removed dim options from class

--- arbok_inspector/classes/dim.py
+++ arbok_inspector/classes/dim.py
@@ -1,35 +1,20 @@
-"""Module for the Dim class."""
+"""Module for the Dim class representing a data dimension."""
 from __future__ import annotations
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from nicegui.elements.html import Html
-    from nicegui.elements.select import Select
-    from nicegui.elements.slider import Slider
+
+AXIS_OPTIONS = ['average', 'select_value', 'y-axis', 'x-axis']
+
 
 class Dim:
-    """
-    Class representing a dimension of the data
-    """
-    def __init__(self, name):
-        """
-       Constructor for Dim class
-        
-        Args:
-            name (str): Name of the dimension
-            
-        Attributes:
-            name (str): Name of the dimension
-            option (str): Option for the dimension (average, select_value, x-axis, y-axis)
-            select_index (int): Index of the selected value for select_value option
-            ui_selector: Reference to the UI element for the dimension
-        """
+    """Pure data class representing a dimension and its current role in plotting."""
+
+    def __init__(self, name: str):
         self.name: str = name
         self.option: str | None = None
         self.select_index: int = 0
-        self.ui_selector: Select | None = None
-        self.slider: Slider | None = None
-        self.select_label: Html | None = None
 
     def __str__(self):
         return self.name
+
+    def __repr__(self):
+        return f"Dim(name={self.name!r}, option={self.option!r}, select_index={self.select_index})"
